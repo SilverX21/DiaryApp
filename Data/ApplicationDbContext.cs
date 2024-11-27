5851ad39d7da10b15f81081b40fcc7d139f109e8
SilverX21 <nuno21araujo95@gmail.com>
2024-11-27 17:35:53 +0000

    Added: database db context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    //steps to create a table using ef core:
+    /*
+     1. create a class
+     2. add DB set
+     3. add-migration Name_of_the_migration
+     4. update-database
+     */
+
+    public DbSet<DiaryEntry> DiaryEntries { get; set; }
+}
---
+++ Models/DiaryEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models;
+
+public class DiaryEntry
+{
+    // [Key] -> EF Core knows by default that if you have a prop named Id that it is the Key  (PK). You use the Key data annotation if the name of the Id would be different, ex: DiaryEntryId
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public string Content { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+}
